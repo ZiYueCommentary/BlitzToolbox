417e1cec866bb2349e06365f73e106d689991c5a
子悦解说 <ziyuecommentary@gmail.com>
2022-11-20 21:36:27 +0800

    fix bugs

--- BlitzToolbox/BlitzToolbox.cpp
+++ BlitzToolbox/BlitzToolbox.cpp
@@ -1,13 +1,18 @@
-#define UNORDERED_MAP
+﻿#define UNORDERED_MAP
 
 #include "../IniControler/IniControler.cpp"
+#include <atlstr.h>
 
 BLITZ3D(BBStr) FindSCP294Drink(BBStr file, BBStr drink) {
-	for (auto drink1 : IniBuffer[file]) { // crazy for each
-		vector<string> drinks = BlitzToolbox::split_string(drink1.first, "|");
-		for (string drink2 : drinks) {
-			if (drink2 == drink) return getCharPtr(drink1.first);
+	auto& buffer = IniBuffer[file];
+	for (auto section = buffer.begin(); section != buffer.end(); section++) {
+		vector<string> vec = BlitzToolbox::split_string(section->first, "|");
+		for (string drink1 : vec) {
+			CString drink2 = drink1.c_str();
+			if (drink2.MakeLower() == CString(drink).MakeLower()) {
+				return getCharPtr(section->first + ","s + drink1);
+			}
 		}
 	}
-	return "Null"; // nope :(
+	return "Null"; // nothing found ¯\_(ツ)_/¯
 }
