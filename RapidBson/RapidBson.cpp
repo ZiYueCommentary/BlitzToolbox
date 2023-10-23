3db0df9580cf1c9ca4764fec88cbce4b57f71bd6
子悦解说 <ziyuecommentary@gmail.com>
2023-10-23 23:44:19 +0800

    RapidBson: Array json support

--- RapidBson/RapidBson.cpp
+++ RapidBson/RapidBson.cpp
@@ -2,7 +2,7 @@
 * RapidBson - A part of BlitzToolbox
 * A fast JSON parser/generator for Blitz3D with both SAX/DOM style API.
 *
-* v1.0 2023.6.22
+* v1.0 2023.10.3
 */
 
 ///////////////////////////
@@ -18,6 +18,7 @@
 #include <string>
 #include <Windows.h>
 #include "../BlitzToolbox.hpp"
+#include <functional>
 
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
@@ -34,21 +35,6 @@ typedef void* Object;
 struct DocumentObj {
     BBStr identifier = DOCUMENT_IDENT;
     Document document;
-    StringBuffer* prettyBuffer;
-    PrettyWriter<StringBuffer>* prettyWriter;
-
-    bool IsNull() {
-        return document.IsNull();
-    }
-
-    DocumentObj() {
-        prettyBuffer = new StringBuffer();
-        prettyWriter = new PrettyWriter<StringBuffer>(*prettyBuffer);
-    }
-
-    ~DocumentObj() {
-        delete prettyBuffer, prettyWriter;
-    }
 };
 
 BLITZ3D(DocumentObj*) JsonParseFromFile(BBStr path) {
@@ -91,16 +77,30 @@ BLITZ3D(int) JsonIsFloat(Value* value) {
     return value->IsFloat();
 }
 
-BLITZ3D(int) JsonIsBoolean(Value* value) {
+BLITZ3D(int) JsonIsBool(Value* value) {
     return value->IsBool();
 }
 
-BLITZ3D(int) JsonIsArray(Value* value) {
-    return value->IsArray();
+BLITZ3D(int) JsonIsArray(Object* obj) {
+    DocumentObj* doc = reinterpret_cast<DocumentObj*>(obj);
+    if (doc->identifier == DOCUMENT_IDENT) { // obj is a document
+        return doc->document.IsArray();
+    }
+    else { // obj is a value
+        Value* val = reinterpret_cast<Value*>(obj);
+        return val->IsArray();
+    }
 }
 
-BLITZ3D(int) JsonIsObject(Value* value) {
-    return value->IsObject();
+BLITZ3D(int) JsonIsObject(Object* obj) {
+    DocumentObj* doc = reinterpret_cast<DocumentObj*>(obj);
+    if (doc->identifier == DOCUMENT_IDENT) { // obj is a document
+        return doc->document.IsObject();
+    }
+    else { // obj is a value
+        Value* val = reinterpret_cast<Value*>(obj);
+        return val->IsObject();
+    }
 }
 
 BLITZ3D(int) JsonIsNull(Object obj) {
@@ -127,12 +127,19 @@ BLITZ3D(float) JsonGetFloat(Value* value) {
     return SAFE_TYPE(value, Float);
 }
 
-BLITZ3D(int) JsonGetBoolean(Value* value) {
+BLITZ3D(int) JsonGetBool(Value* value) {
     return SAFE_TYPE(value, Bool);
 }
 
-BLITZ3D(Array*) JsonGetArray(Value* value) {
-    return value->IsArray() ? new Array(value->GetArray()) : nullptr;
+BLITZ3D(Array*) JsonGetArray(Object* obj) {
+    DocumentObj* doc = reinterpret_cast<DocumentObj*>(obj);
+    if (doc->identifier == DOCUMENT_IDENT) { // obj is a document
+        return new Array(doc->document.GetArray());
+    }
+    else { // obj is a value
+        Value* val = reinterpret_cast<Value*>(obj);
+        return new Array(val->GetArray());
+    }
 }
 
 BLITZ3D(int) JsonGetArraySize(Array* array) {
